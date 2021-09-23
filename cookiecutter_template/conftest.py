6acc402df76eeab4506568f40c82129e67b18d45
Schweigs <hello@compaz.io>
2021-09-23 19:05:08 +0100

    initial commit

---
+++ cookiecutter_template/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cookiecutter_template.users.models import User
+from cookiecutter_template.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
